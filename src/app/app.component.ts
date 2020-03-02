c257bf79b6a340d1444165886abceb43a84f2d42
masterh4cker <tabathafoes@gmail.com>
2020-03-02 10:13:38 -0700

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// debugger;
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'app';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+//import { AppComponent } from './app.component';
+import { ProductComponent } from "./component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    declarations: [ProductComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [ProductComponent]
 })
 export class AppModule { }
+
